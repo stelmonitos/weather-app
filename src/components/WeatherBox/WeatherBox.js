98ac27c6dc5ec4eeae741bec8d649b994d6cc6a9
Jakub Stelmasiak <stelmon3@gmail.com>
2024-02-24 18:01:16 +0100

    add error

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -3,18 +3,24 @@ import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
 import { useCallback } from 'react';
 import { useState } from 'react';
+import ErrorBox from '../ErrorBox/ErrorBox';
 
 const WeatherBox = props => {
   const [weather, setWeather] = useState('');
 
   const [pending, setPending] = useState(false);
 
+  const [error, setError] = useState(false);
+
   const handleCityChange = useCallback((city) => {
     setPending(true);
+    setError(false);
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0a9c3691799c5c79f3a676a72319f2c8&units=metric`)
-      .then(res => res.json())
-      .then(data => {
-        setPending(false);
+    .then(res => {
+      if(res.status === 200) {
+        return res.json()
+          .then(data => {
+          setPending(false);
         const weatherData = {
           city: data.name,
           temp: data.main.temp,
@@ -22,14 +28,19 @@ const WeatherBox = props => {
           description: data.weather[0].main
         };
         setWeather(weatherData); // Update local state with weatherData
-      });
+      })
+    } else {
+      setError(true);
+    }
+  });
   }, []);
 
   return (
     <section>
       <PickCity action={handleCityChange} />
-      {weather && <WeatherSummary {...weather} />}
-      {pending && <Loader />}
+      {(weather && !pending) && <WeatherSummary {...weather} />}
+      {error && <ErrorBox><strong>There is not such city</strong></ErrorBox>}
+      {pending && !error && <Loader />}
     </section>
   )
 };
